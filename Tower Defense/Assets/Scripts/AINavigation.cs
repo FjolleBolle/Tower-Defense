df3ae1d6e88c535f6b38a72b315acf85f4154e37
FjolleBolle <mschj20@student.sdu.dk>
2023-11-20 19:52:24 +0100

    I got some shit that works babyyyyyy

--- Tower Defense/Assets/Scripts/AINavigation.cs
+++ Tower Defense/Assets/Scripts/AINavigation.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.AI.Navigation;
 using UnityEngine;
 using UnityEngine.AI;
 
@@ -8,12 +9,16 @@ public class AINavigation : MonoBehaviour
     private NavMeshAgent agent;
     public GameObject goal;
 
+    public GameObject costChangePrefab;
+    public NavMeshSurface surface;
 
+    public bool changeCost = false;
 
     private void Awake()
     {
         goal = GameObject.FindGameObjectWithTag("Base");
         agent = gameObject.GetComponent<NavMeshAgent>();
+        surface.BuildNavMesh();
     }
 
     // Start is called before the first frame update
@@ -25,6 +30,18 @@ public class AINavigation : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if(Input.GetKeyDown(KeyCode.Space) && changeCost == true)
+        {
+            changeCost = false;
+            StartCoroutine(PlaceCostField());
+        }
+    }
 
+    IEnumerator PlaceCostField()
+    {
+        Instantiate(costChangePrefab, new Vector3(this.gameObject.transform.position.x, this.gameObject.transform.position.y, this.gameObject.transform.position.z), Quaternion.identity);
+        yield return new WaitForSeconds(0.5f);
+        surface.BuildNavMesh();
+        changeCost = true;
     }
 }
