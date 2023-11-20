d210fb4133b68bf8a4791f37e9d0cc51a61529f8
FjolleBolle <mschj20@student.sdu.dk>
2023-11-20 11:57:56 +0100

    Prøvet at lave grid

---
+++ Tower Defense/Assets/Scripts/GridCreator.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridCreator : MonoBehaviour
+{
+    public int columnLength;
+    public int rowLength;
+
+    public float x_Space;
+    public float z_Space;
+
+    public GameObject prefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        for(int i = 0; i < columnLength*rowLength; i++)
+        {
+            Instantiate(prefab, new Vector3(x_Space + (x_Space * (i % columnLength)), 0, z_Space + (z_Space * (i / columnLength))), Quaternion.identity);
+        }
+    }
+}
