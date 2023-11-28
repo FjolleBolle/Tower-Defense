60f2dbf67300b731f9949ef464d765d29edc5ce7
FjolleBolle <mschj20@student.sdu.dk>
2023-11-28 11:50:27 +0100

    Timer i spawner

--- Tower Defense/Assets/Scripts/Spawner.cs
+++ Tower Defense/Assets/Scripts/Spawner.cs
@@ -20,12 +20,32 @@ public class Spawner : MonoBehaviour
     public Transform point1;
     public Transform point2;
 
+    public float timer;
+    public List<float> prevTime;
+
+    public bool hasSpawned = false;
+
     // Start is called before the first frame update
     void Start()
     {
         enemyCounter = 0;
+        prevTime = new List<float>();
         StartCoroutine(spawnEnemy(meleeInterval, melee, chosenPoint));
         //StartCoroutine(spawnEnemy(rangerInterval, ranger));
+    }
+
+    private void Update()
+    {
+        if (enemyCounter >= 1)
+        {
+            timer += Time.deltaTime;
+        }
+        else if(enemyCounter <= 0 && hasSpawned == true)
+        {
+            hasSpawned = false;
+            prevTime.Add(timer);
+            timer = 0;
+        }
     }
 
     private IEnumerator spawnEnemy(float interval, GameObject enemy, Transform point)
@@ -42,16 +62,18 @@ public class Spawner : MonoBehaviour
             {
                 chosenPoint = point1;
             }
+            yield return new WaitForSeconds(1);
         }
 
         point = chosenPoint;
 
         if(enemyCounter < 5)
         {
+            hasSpawned = true;
             GameObject newEnemy = Instantiate(enemy, new Vector3(point.position.x, point.position.y, point.position.z), Quaternion.identity);
             enemyCounter++;
         }
-
+        Debug.Log("Done");
         StartCoroutine(spawnEnemy(interval, enemy, point));
     }
 }
