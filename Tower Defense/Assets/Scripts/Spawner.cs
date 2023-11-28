e5aa9cfebc6ebf4bfaaf44eb877924f6bdad2a9a
FjolleBolle <mschj20@student.sdu.dk>
2023-11-28 10:57:23 +0100

    Flere spawnpoint og 5 enemy wave

--- Tower Defense/Assets/Scripts/Spawner.cs
+++ Tower Defense/Assets/Scripts/Spawner.cs
@@ -9,22 +9,49 @@ public class Spawner : MonoBehaviour
     //[SerializeField]
     //private GameObject ranger;
 
-    [SerializeField]
+    [SerializeField] 
     private float meleeInterval = 5f;
     //[SerializeField]
     //private float rangerInterval = 10f;
 
+    public int enemyCounter = 0;
+
+    public Transform chosenPoint;
+    public Transform point1;
+    public Transform point2;
+
     // Start is called before the first frame update
     void Start()
     {
-        StartCoroutine(spawnEnemy(meleeInterval, melee));
+        enemyCounter = 0;
+        StartCoroutine(spawnEnemy(meleeInterval, melee, chosenPoint));
         //StartCoroutine(spawnEnemy(rangerInterval, ranger));
     }
 
-    private IEnumerator spawnEnemy(float interval, GameObject enemy)
+    private IEnumerator spawnEnemy(float interval, GameObject enemy, Transform point)
     {
         yield return new WaitForSeconds(interval);
-        GameObject newEnemy = Instantiate(enemy, new Vector3(gameObject.transform.position.x, gameObject.transform.position.y, gameObject.transform.position.z), Quaternion.identity);
-        StartCoroutine(spawnEnemy(interval, enemy));
+        if (GameObject.FindGameObjectsWithTag("Melee").Length <= 0)
+        {
+            enemyCounter = 0;
+            if (chosenPoint == point1)
+            {
+                chosenPoint = point2;
+            }
+            else if (chosenPoint == point2)
+            {
+                chosenPoint = point1;
+            }
+        }
+
+        point = chosenPoint;
+
+        if(enemyCounter < 5)
+        {
+            GameObject newEnemy = Instantiate(enemy, new Vector3(point.position.x, point.position.y, point.position.z), Quaternion.identity);
+            enemyCounter++;
+        }
+
+        StartCoroutine(spawnEnemy(interval, enemy, point));
     }
 }
